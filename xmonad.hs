343875706485b719e24149ecd2705e36de941a63
Leif Warner <abimelech@gmail.com>
2009-08-30 15:29:20 -0700

    Removed border on fullscreen in XMonad

--- xmonad.hs
+++ xmonad.hs
@@ -1,9 +1,10 @@
 import XMonad
 import XMonad.Util.EZConfig(additionalKeys)
+import XMonad.Layout.NoBorders
 
 myTerm = "urxvt +sb"
 
-main = xmonad $ defaultConfig {terminal = myTerm} 
+main = xmonad $ defaultConfig {terminal = myTerm, layoutHook = smartBorders $ layoutHook defaultConfig} 
                 `additionalKeys` 
                 [ ((0, xK_Super_L), spawn "exe=`dmenu_path | dmenu` && eval \"exec $exe\"") ,
                  ((0, xK_Menu), spawn myTerm) ]
