ab401abb8e3aab11a992a6cdfafab13cfe92e37b
Leif Warner <abimelech@gmail.com>
2010-04-21 23:32:12 -0700

    Don't need shift on XMonad goto, added Turtle syntax highlighting

--- xmonad.hs
+++ xmonad.hs
@@ -11,8 +11,8 @@ main = xmonad $ defaultConfig {terminal = myTerm, layoutHook = smartBorders $ la
                 `additionalKeys` 
                 [ ((0, xK_Super_L), spawn "exe=`dmenu_path | dmenu` && eval \"exec $exe\"") ,
                   ((0, xK_Menu), spawn myTerm) ,
-                  ((mod1Mask .|. shiftMask, xK_g), windowPromptGoto  defaultXPConfig { autoComplete = Just 500000 } ) ,
-                  ((mod1Mask .|. shiftMask, xK_b), windowPromptBring defaultXPConfig ) ,
-                  ((0, xK_F7), lowerVolumeChannels ["PCM"] 4 >>= flip osdCat defaultOSDOpts),
-                  ((0, xK_F8), raiseVolumeChannels ["PCM"] 4 >>= flip osdCat defaultOSDOpts)
+                  ((mod1Mask, xK_g), windowPromptGoto  defaultXPConfig { autoComplete = Just 500000 } ) ,
+                  ((mod1Mask, xK_b), windowPromptBring defaultXPConfig ) ,
+                  ((0, xK_F7), lowerVolumeChannels ["Master"] 4 >>= flip osdCat (\_->"")),
+                  ((0, xK_F8), raiseVolumeChannels ["Master"] 4 >>= flip osdCat (\_->""))
                 ]
