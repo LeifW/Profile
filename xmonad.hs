06fed96b49476eeb6f515cc55971ccebff03c6f1
Leif Warner <abimelech@gmail.com>
2011-02-12 21:31:09 -0800

    xmobar status display, git color

--- xmonad.hs
+++ xmonad.hs
@@ -1,22 +1,38 @@
 import XMonad
-import XMonad.Util.EZConfig(additionalKeys)
+import XMonad.Util.EZConfig (additionalKeys)
+import XMonad.Util.Run (spawnPipe)
+import XMonad.Hooks.DynamicLog
 import XMonad.Layout.NoBorders
 import XMonad.Prompt (defaultXPConfig, autoComplete)
 import XMonad.Prompt.Window (windowPromptGoto, windowPromptBring)
 import XMonad.Actions.Volume (raiseVolumeChannels, lowerVolumeChannels, osdCat, defaultOSDOpts)
-import Data.Ratio((%))
+import System.IO (hPutStrLn)
+--import Data.Ratio ((%))
+import XMonad.Layout.ResizableTile
 
 myTerm = "urxvt +sb"
 
 myLayout = Mirror tall ||| tall ||| Full
-  where tall = Tall {tallNMaster = 1, tallRatioIncrement = 3 % 100, tallRatio = 1 % 2}
+  where tall = ResizableTall 1 (3/100) (1/2) [] --Tall {tallNMaster = 1, tallRatioIncrement = 3 % 100, tallRatio = 1 % 2}
+        golden = toRational (2/(1 + sqrt 5 :: Double))
 
-main = xmonad $ defaultConfig {terminal = myTerm, layoutHook = smartBorders myLayout} 
+main = do 
+    xmproc <- spawnPipe "/usr/bin/xmobar /home/leif/.xmobarrc"
+    xmonad $ defaultConfig {
+             terminal = myTerm, 
+             layoutHook = smartBorders myLayout,
+             logHook = dynamicLogWithPP $ xmobarPP
+                        { ppOutput = hPutStrLn xmproc,
+                          ppTitle = xmobarColor "green" "" . shorten 70
+                        }
+             } 
                 `additionalKeys` 
                 [ ((0, xK_Super_L), spawn "exe=`dmenu_path | dmenu` && eval \"exec $exe\"") ,
                   ((0, xK_Menu), spawn myTerm) ,
                   ((mod1Mask, xK_g), windowPromptGoto  defaultXPConfig { autoComplete = Just 500000 } ) ,
                   ((mod1Mask, xK_b), windowPromptBring defaultXPConfig ) ,
                   ((0, xK_F7), lowerVolumeChannels ["Master"] 4 >>= flip osdCat (\_->"")),
-                  ((0, xK_F8), raiseVolumeChannels ["Master"] 4 >>= flip osdCat (\_->""))
+                  ((0, xK_F8), raiseVolumeChannels ["Master"] 4 >>= flip osdCat (\_->"")),
+                  ((mod1Mask, xK_a), sendMessage MirrorShrink),
+                  ((mod1Mask, xK_z), sendMessage MirrorExpand)
                 ]
