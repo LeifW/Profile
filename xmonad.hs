777876e54b50c1502a084ee331e7c7133a90afad
Leif Warner <leif@janrain.com>
2011-12-04 03:42:38 -0800

    ResizeableTile, safeSpawn in XMonad.

--- xmonad.hs
+++ xmonad.hs
@@ -1,41 +1,39 @@
 import XMonad
 import XMonad.Util.EZConfig (additionalKeys)
 import XMonad.Hooks.DynamicLog
-import XMonad.Layout.NoBorders
+import XMonad.Layout.NoBorders (smartBorders)
+import XMonad.Layout.ResizableTile
 import XMonad.Prompt (defaultXPConfig, autoComplete)
 import XMonad.Prompt.Window (windowPromptGoto, windowPromptBring)
-import XMonad.Actions.Volume (raiseVolumeChannels, lowerVolumeChannels, osdCat, defaultOSDOpts)
-import Data.Ratio ((%))
-import XMonad.Layout.ResizableTile
+import XMonad.Actions.Volume (raiseVolumeChannels, lowerVolumeChannels, osdCat)
+import XMonad.Util.Run (safeSpawnProg)
 import Graphics.X11.ExtraTypes
-import Network.MPD
-import Network.MPD.Commands.Extensions
-import Control.Monad (void)
-
-myTerm = "urxvtc"
-
-myLayout = tall ||| Mirror tall ||| Full
-  where tall = Tall {tallNMaster = 1, tallRatioIncrement = 3 % 100, tallRatio = 1 % 2}
-
-mpd = io . void . withMPD
 
 myConfig = defaultConfig {
-             terminal = myTerm, 
+             terminal = "urxvtc", 
              layoutHook = smartBorders myLayout
            }
            `additionalKeys`
-           [ ((0, xK_Super_L), spawn "dmenu_run") ,
-             ((mod1Mask, xK_p), spawn "dmenu_run") , -- override of default
-             ((0, xK_Menu), spawn myTerm) ,
+           [ ((0, xK_Super_L), safeSpawnProg "dmenu_run") ,
+             ((mod1Mask, xK_p), safeSpawnProg "dmenu_run") , -- override of default
+             ((0, xK_Menu), safeSpawnProg $ terminal myConfig) ,
+             ((mod1Mask, xK_a), sendMessage MirrorShrink) ,
+             ((mod1Mask, xK_z), sendMessage MirrorExpand) ,
+             ((0, xK_F7), vol lowerVolumeChannels 2), 
+             ((0, xK_F8), vol raiseVolumeChannels 2),
+             ((0, xF86XK_AudioLowerVolume), vol lowerVolumeChannels 2), 
+             ((0, xF86XK_AudioRaiseVolume), vol raiseVolumeChannels 2),
              ((mod1Mask, xK_g), windowPromptGoto  defaultXPConfig { autoComplete = Just 500000 } ) ,
-             ((mod1Mask, xK_b), windowPromptBring defaultXPConfig ) ,
-             ((0, xF86XK_AudioLowerVolume), lowerVolumeChannels ["Master"] 3 >>= flip osdCat (\_->"")),
-             ((0, xF86XK_AudioRaiseVolume), raiseVolumeChannels ["Master"] 3 >>= flip osdCat (\_->"")),
-             ((mod1Mask, xK_b), mpd previous ) ,
-             ((mod1Mask, xK_n), mpd next )
+             ((mod1Mask, xK_b), windowPromptBring defaultXPConfig )
            ]
+  where
+    myLayout = tall ||| Mirror tall ||| Full
+      where tall = ResizableTall { _nmaster = 1,  _delta = 3/100, _frac = 1/2, _slaves = [] }
+    vol action n = do
+      v <- action ["Master"] n 
+      osdCat v (\_->"")
 
-statusBarSettings = xmobarPP { ppTitle = xmobarColor "green" "" . shorten 70 }
+statusBarSettings = xmobarPP { ppTitle = xmobarColor "green" ""}
 toggleStrutsKey _ = (mod1Mask, xK_s)
 
 main = xmonad =<< statusBar "xmobar" statusBarSettings toggleStrutsKey myConfig
