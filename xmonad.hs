f4b4c02332227812b9c6f618c6fed224c67a1090
Leif Warner <abimelech@gmail.com>
2017-09-29 17:37:02 -0700

    Switch to navigation2D; more top-level bindings.

--- xmonad.hs
+++ xmonad.hs
@@ -4,49 +4,46 @@ import XMonad.Hooks.DynamicLog
 import XMonad.Hooks.EwmhDesktops
 import XMonad.Layout.NoBorders (smartBorders)
 import XMonad.Layout.ResizableTile
-import XMonad.Prompt (defaultXPConfig, autoComplete)
+import XMonad.Prompt (autoComplete)
 import XMonad.Prompt.Window (windowPromptGoto, windowPromptBring)
 import XMonad.Actions.FindEmptyWorkspace (viewEmptyWorkspace, tagToEmptyWorkspace)
 import XMonad.Prompt.Ssh (sshPrompt)
-import XMonad.Util.Run (safeSpawn, safeSpawnProg)
+import XMonad.Util.Run (safeSpawn)
 import Graphics.X11.ExtraTypes
-import XMonad.Hooks.SetWMName
-import XMonad.Actions.WindowNavigation
+import XMonad.Actions.Navigation2D
 import XMonad.Actions.GridSelect
-import Control.Monad (void)
 
-myMask :: KeyMask
-myMask = mod4Mask
+winKey :: KeyMask
+winKey = mod4Mask
+
+(|=>) :: k -> a -> (k, a)
+k |=> a = (k, a)
+
+win, winShift, plain :: a -> (KeyMask, a)
+win k = (winKey, k)
+winShift k = (winKey .|. shiftMask, k)
+plain k = (0, k)
 
 keyBindings :: [((ButtonMask, KeySym), X ())]
 keyBindings =
-  plain xK_Print |=> safeSpawnProg "dmenu_run" :
-  mod xK_p |=> safeSpawnProg "dmenu_run" : -- override of default
-  plain xK_Menu |=> safeSpawnProg (terminal myConfig) :
-  mod xK_a |=> sendMessage MirrorShrink :
-  mod xK_z |=> sendMessage MirrorExpand :
-  mod xK_r |=> sshPrompt defaultXPConfig :
-  mod xK_g |=> windowPromptGoto defaultXPConfig { autoComplete = Just 500000 } :
-  modShift xK_g |=> windowPromptBring defaultXPConfig :
-  mod xK_f |=> goToSelected defaultGSConfig :
-  mod xK_m |=> viewEmptyWorkspace :
-  modShift xK_m |=> tagToEmptyWorkspace :
-  plain xF86XK_MonBrightnessUp |=> safeSpawn "xbacklight" ["-inc", "2"] :
-  plain xF86XK_MonBrightnessDown |=> safeSpawn "xbacklight" ["-dec", "2"] :
+  win xK_a |=> sendMessage MirrorShrink :
+  win xK_z |=> sendMessage MirrorExpand :
+  win xK_r |=> sshPrompt def :
+  win xK_g |=> windowPromptGoto def { autoComplete = Just 500000 } :
+  winShift xK_g |=> windowPromptBring def :
+  win xK_f |=> goToSelected def :
+  win xK_m |=> viewEmptyWorkspace :
+  winShift xK_m |=> tagToEmptyWorkspace :
+  plain xF86XK_MonBrightnessUp |=> safeSpawn "xbacklight" ["-inc", "1"] :
+  plain xF86XK_MonBrightnessDown |=> safeSpawn "xbacklight" ["-dec", "1"] :
   []
-  where
-    k |=> a = (k, a)
-    mod k = (myMask, k)
-    modShift k = (myMask .|. shiftMask, k)
-    plain k = (0, k)
 
 myConfig =
-  ewmh $ defaultConfig {
+  ewmh $ def {
     terminal = "urxvtc", 
-    modMask = myMask,
+    modMask = winKey,
     layoutHook = smartBorders myLayout,
-    handleEventHook = handleEventHook defaultConfig <+> fullscreenEventHook
-    --startupHook = setWMName "LG3D"
+    handleEventHook = handleEventHook def <+> fullscreenEventHook
   } 
   `additionalKeys`
   keyBindings
@@ -54,12 +51,18 @@ myConfig =
     myLayout = tall ||| Mirror tall ||| Full
     tall = ResizableTall { _nmaster = 1,  _delta = 3/100, _frac = 1/2, _slaves = [] }
 
-main :: IO ()
-main =
-  statusBar "xmobar" statusBarSettings toggleStrutsKey myConfig >>=
-  withWindowNavigation (xK_Up, xK_Left, xK_Down, xK_Right) >>=
-  xmonad
-    where
-      statusBarSettings = xmobarPP { ppTitle = xmobarColor "green" ""}
-      toggleStrutsKey _ = (myMask, xK_s)
+navSetting :: XConfig a -> XConfig a
+navSetting =
+  navigation2D
+    def
+    (xK_Up, xK_Left, xK_Down, xK_Right)
+    [
+      win windowGo,
+      winShift windowSwap
+    ]
+    False
 
+main :: IO ()
+main = do
+  configWithXmobar <- xmobar myConfig
+  xmonad $ navSetting configWithXmobar
